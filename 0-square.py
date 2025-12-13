860603a079b69372ea76c19f1fbda45f326d53cb
SÉsalahliaygul9-jpg <salahliaygul9@gmial.com>
2025-12-13 17:01:16 +0000

    Square with size

---
+++ 0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size):
+        """Initialize the square with a private size attribute"""
+        self.__size = size
