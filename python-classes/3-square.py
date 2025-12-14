bdf7628a18f11a1c75820de10ba395017e3aabd1
salahliaygul9-jpg <salahliaygul9@gmail.com>
2025-12-14 04:35:16 +0000

    Access and update private attribute

---
+++ python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieves the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size * self.__size
